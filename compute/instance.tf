cb593c7ef836f742fe8aec054b7d55d871817af4
Ayush Garg <ayushgarg8875@gmail.com>
2025-07-11 08:53:03 +0000

    Add compute Terraform module

---
+++ compute/instance.tf
@@ -0,0 +1,23 @@
+resource "oci_core_instance" "vm_instance" {
+  availability_domain = var.availability_domain
+  compartment_id      = var.compartment_ocid
+  shape               = var.instance_shape
+
+  create_vnic_details {
+    subnet_id        = var.subnet_id
+    assign_public_ip = true
+    hostname_label   = var.hostname
+  }
+
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key_path)
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.image_id
+  }
+
+  display_name = var.hostname
+}
+
---
+++ compute/variables.tf
@@ -0,0 +1,14 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+variable "availability_domain" {}
+variable "subnet_id" {}
+variable "hostname" {}
+variable "image_id" {}
+variable "ssh_public_key_path" {}
+variable "instance_shape" {}
+
